07145fafe57316df7fc31ce0ba4ae0329e6fdda9
ppzxc <cjh8487@naver.com>
2025-10-02 17:39:07 +0900

    2025-10-02 fixed - remove getter setter method - add getter setter method on lombok

--- src/main/java/io/github/ppzxc/fq/MVStoreFileQueueProperties.java
+++ src/main/java/io/github/ppzxc/fq/MVStoreFileQueueProperties.java
@@ -1,7 +1,11 @@
 package io.github.ppzxc.fq;
 
 import java.util.concurrent.TimeUnit;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MVStoreFileQueueProperties {
 
   private String fileName;
@@ -18,116 +22,4 @@ public class MVStoreFileQueueProperties {
   private boolean fair = true;
   private int tryLockTimeout = 1;
   private TimeUnit tryLockTimeunit = TimeUnit.SECONDS;
-
-  public String getFileName() {
-    return fileName;
-  }
-
-  public void setFileName(String fileName) {
-    this.fileName = fileName;
-  }
-
-  public String getQueueName() {
-    return queueName;
-  }
-
-  public void setQueueName(String queueName) {
-    this.queueName = queueName;
-  }
-
-  public int getBatchSize() {
-    return batchSize;
-  }
-
-  public void setBatchSize(int batchSize) {
-    this.batchSize = batchSize;
-  }
-
-  public long getMaxSize() {
-    return maxSize;
-  }
-
-  public void setMaxSize(long maxSize) {
-    this.maxSize = maxSize;
-  }
-
-  public int getAutoCompactFillRate() {
-    return autoCompactFillRate;
-  }
-
-  public void setAutoCompactFillRate(int autoCompactFillRate) {
-    this.autoCompactFillRate = autoCompactFillRate;
-  }
-
-  public boolean isUseCompress() {
-    return useCompress;
-  }
-
-  public void setUseCompress(boolean useCompress) {
-    this.useCompress = useCompress;
-  }
-
-  public boolean isAutoCommitDisabled() {
-    return autoCommitDisabled;
-  }
-
-  public void setAutoCommitDisabled(boolean autoCommitDisabled) {
-    this.autoCommitDisabled = autoCommitDisabled;
-  }
-
-  public int getAutoCommitBufferSize() {
-    return autoCommitBufferSize;
-  }
-
-  public void setAutoCommitBufferSize(int autoCommitBufferSize) {
-    this.autoCommitBufferSize = autoCommitBufferSize;
-  }
-
-  public int getMaxRetry() {
-    return maxRetry;
-  }
-
-  public void setMaxRetry(int maxRetry) {
-    this.maxRetry = maxRetry;
-  }
-
-  public long getRetryDelay() {
-    return retryDelay;
-  }
-
-  public void setRetryDelay(long retryDelay) {
-    this.retryDelay = retryDelay;
-  }
-
-  public int getCacheSize() {
-    return cacheSize;
-  }
-
-  public void setCacheSize(int cacheSize) {
-    this.cacheSize = cacheSize;
-  }
-
-  public boolean isFair() {
-    return fair;
-  }
-
-  public void setFair(boolean fair) {
-    this.fair = fair;
-  }
-
-  public int getTryLockTimeout() {
-    return tryLockTimeout;
-  }
-
-  public void setTryLockTimeout(int tryLockTimeout) {
-    this.tryLockTimeout = tryLockTimeout;
-  }
-
-  public TimeUnit getTryLockTimeunit() {
-    return tryLockTimeunit;
-  }
-
-  public void setTryLockTimeunit(TimeUnit tryLockTimeunit) {
-    this.tryLockTimeunit = tryLockTimeunit;
-  }
 }
