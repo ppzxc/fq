fb66158b33b22d72f0e4913bfd2a45f0deec3833
ppzxc <cjh8487@naver.com>
2025-12-10 17:29:10 +0900

    2025-12-10 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     `java-library`
-    jacoco
-    `maven-publish`
 }
 
 group = providers.gradleProperty("GROUP_NAME").get()
@@ -32,23 +30,4 @@ tasks.test {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
 }
