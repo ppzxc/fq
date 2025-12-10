70b90937e912a0d18b54225067455b1a3c084526
ppzxc <cjh8487@naver.com>
2025-12-10 17:25:30 +0900

    2025-12-10 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     `java-library`
-    signing
     jacoco
-    alias(libs.plugins.net.thebugmc.gradle.sonatype.central.portal.publisher)
+    `maven-publish`
 }
 
 group = providers.gradleProperty("GROUP_NAME").get()
@@ -33,81 +32,23 @@ tasks.test {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-    withSourcesJar()
-    withJavadocJar()
 }
 
-artifacts {
-    archives(tasks.named("javadocJar"))
-    archives(tasks.named("sourcesJar"))
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.required = true
-//        xml.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-        csv.required = true
-//        csv.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-        html.required = true
-//        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-    }
-    finalizedBy(tasks.jacocoTestCoverageVerification)
-}
-
-tasks.jacocoTestCoverageVerification {
-    violationRules {
-        rule {
-            element = "CLASS"
-
-            limit {
-                counter = "BRANCH"
-                value = "COVEREDRATIO"
-                minimum = BigDecimal.valueOf(0.50)
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
-}
 
-signing {
-    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
-    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
-
-    if (signingKey.isPresent && signingPassphrase.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-        val extension = extensions.getByName("publishing") as PublishingExtension
-        sign(extension.publications)
-    }
-}
-
-centralPortal {
-    username = providers.environmentVariable("OSSRH_USERNAME").getOrElse("null")
-    password = providers.environmentVariable("OSSRH_PASSWORD").getOrElse("null")
-    pom {
-        group = providers.gradleProperty("GROUP_NAME").get()
-        name = providers.gradleProperty("ARTIFACT_NAME").get()
-        description = providers.gradleProperty("POM_DESCRIPTION").get()
-        packaging = "jar"
-        // `name = project.name`, `description = project.description` and `packaging = "jar"`
-        // are applied automatically, but you can override them here
-        url = providers.gradleProperty("POM_URL").get()
-        licenses {
-            license {
-                name = providers.gradleProperty("POM_LICENSE_NAME").get()
-                url = providers.gradleProperty("POM_LICENSE_URL").get()
-            }
-        }
-        developers {
-            developer {
-                id = providers.gradleProperty("POM_DEVELOPER_ID").get()
-                name = providers.gradleProperty("POM_DEVELOPER_NAME").get()
-                email = providers.gradleProperty("POM_DEVELOPER_EMAIL").get()
-                url = providers.gradleProperty("POM_DEVELOPER_URL").get()
-            }
-        }
-        scm {
-            url = providers.gradleProperty("POM_SCM_URL").get()
-            connection = providers.gradleProperty("POM_SCM_CONNECTION").get()
-            developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION").get()
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
         }
     }
 }
