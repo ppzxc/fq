ba7273176de7224097db00a4951a0364ee073966
ppzxc <cjh8487@naver.com>
2026-01-02 17:49:02 +0900

    0.0.9

--- src/main/java/io/github/ppzxc/fq/FileQueueMessage.java
+++ src/main/java/io/github/ppzxc/fq/FileQueueMessage.java
@@ -1,14 +1,46 @@
 package io.github.ppzxc.fq;
 
-import lombok.Builder;
+import java.io.Serializable;
 import lombok.Getter;
-import lombok.With;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@With
 @Getter
-@Builder
-public class FileQueueMessage {
+@Setter
+@RequiredArgsConstructor
+public class FileQueueMessage implements Serializable {
 
-  private final Class<?> clazz;
-  private final Object message;
+  private static final long serialVersionUID = 1255425631068333713L;
+  private Class<?> clazz;
+  private Object message;
+
+  public static FileQueueMessageBuilder builder() {
+    return new FileQueueMessageBuilder();
+  }
+
+  public static final class FileQueueMessageBuilder {
+
+    private Class<?> clazz;
+    private Object message;
+
+    private FileQueueMessageBuilder() {
+    }
+
+    public FileQueueMessageBuilder clazz(Class<?> clazz) {
+      this.clazz = clazz;
+      return this;
+    }
+
+    public FileQueueMessageBuilder message(Object message) {
+      this.message = message;
+      return this;
+    }
+
+    public FileQueueMessage build() {
+      FileQueueMessage fileQueueMessage = new FileQueueMessage();
+      fileQueueMessage.setClazz(clazz);
+      fileQueueMessage.setMessage(message);
+      return fileQueueMessage;
+    }
+  }
 }
